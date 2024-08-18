17bfba2dc3f43688980372e42b9824b6b4c7c422
Neo677 <111650030+Neo677@users.noreply.github.com>
2024-08-18 12:04:53 +0200

    100% bonus ?

--- server.c
+++ server.c
@@ -3,24 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thobenel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tomtom <tomtom@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 10:01:41 by thobenel          #+#    #+#             */
-/*   Updated: 2024/08/16 10:01:43 by thobenel         ###   ########.fr       */
+/*   Updated: 2024/08/18 12:01:49 by tomtom           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf/ft_printf.h"
 #include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include "printf/ft_printf.h"
+#include <unistd.h>
 #define FIN_TRANSMISSION '\0'
 
 void	ft_handle(int sig)
 {
-	static unsigned char str;
-	static int i;
+	static unsigned char	str;
+	static int				i;
 
 	str |= (sig == SIGUSR1);
 	i++;
@@ -31,17 +32,37 @@ void	ft_handle(int sig)
 		else
 			ft_printf("%c", str);
 		i = 0;
+		str = 0;
 	}
 	else
-		str <<=1;
+		str <<= 1;
 }
 
-int main(void)
+void	ft_baniere(void)
 {
-	ft_printf("Your pid = %d\n", getpid);
+	ft_printf("********************************************\n");
+	ft_printf("*            Welcome to MiniTalk           *\n");
+	ft_printf("********************************************\n");
+	ft_printf("\n\n");
+}
+
+void	ft_pid(void)
+{
+	ft_printf("********************************************\n");
+	ft_printf("*            Serveur PID: %d           *\n", getpid());
+	ft_printf("********************************************\n");
+	ft_printf("\n\n");
+}
+
+int	main(void)
+{
+	ft_baniere();
+	ft_pid();
 	signal(SIGUSR1, ft_handle);
 	signal(SIGUSR2, ft_handle);
-	while(1)
-		pause;
+	ft_printf("\n            Waiting for message...\n");
+	ft_printf("********************************************\n");
+	while (1)
+		pause();
 	return (0);
 }
