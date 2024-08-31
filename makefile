810dcabadb4104b43b8fe9c992a5ac3245b3f4df
thobenel <grandmasterchenn@gmail.com>
2024-08-31 17:33:35 +0200

    85

--- makefile
+++ makefile
@@ -1,9 +1,13 @@
 CC = gcc
 CFLAGS = -Wall -Werror -Wextra -fPIC -no-pie
 
-PRINTF_PATH = ft_printf
-PRINTF_LIB = $(PRINTF_PATH)/ft_printf.a
-PRINTF_INC = -I$(PRINTF_PATH)
+# PRINTF_PATH = ft_printf
+# PRINTF_LIB = $(PRINTF_PATH)/ft_printf.a
+# PRINTF_INC = -I$(PRINTF_PATH)
+
+LIBFT_PATH = libft
+LIBFT_LIB = $(LIBFT_PATH)/libft.a
+LIBFT_INC = -I$(LIBFT_PATH)
 
 SERVER = server.c
 CLIENT = client.c
@@ -12,25 +16,46 @@ CLIENT_EXE = $(CLIENT:.c=.o)
 
 all: server client
 
-server: $(SERVER_EXE) $(PRINTF_LIB)
-	$(CC) $(CFLAGS) $(SERVER_EXE) -o server -L$(PRINTF_PATH) -l:ft_printf.a
+server: $(SERVER_EXE) $(LIBFT_LIB)
+	$(CC) $(CFLAGS) $(SERVER_EXE) -o server -L$(LIBFT_PATH) -l:libft.a
 
-client: $(CLIENT_EXE) $(PRINTF_LIB)
-	$(CC) $(CFLAGS) $(CLIENT_EXE) -o client -L$(PRINTF_PATH) -l:ft_printf.a
+client: $(CLIENT_EXE) $(PRINTF_LIB) $(LIBFT_LIB)
+	$(CC) $(CFLAGS) $(CLIENT_EXE) -o client -L$(LIBFT_PATH) -l:libft.a
+
+
+# server: $(SERVER_EXE) $(PRINTF_LIB) $(LIBFT_LIB)
+# 	$(CC) $(CFLAGS) $(SERVER_EXE) -o server -L$(PRINTF_PATH) -l:ft_printf.a 
+
+# client: $(CLIENT_EXE) $(PRINTF_LIB) $(LIBFT_LIB)
+# 	$(CC) $(CFLAGS) $(CLIENT_EXE) -o client -L$(PRINTF_PATH) -l:ft_printf.a 
+
+# %.o: %.c
+# 	$(CC) $(CFLAGS) $(PRINTF_INC) -c $< -o $@
 
 %.o: %.c
-	$(CC) $(CFLAGS) $(PRINTF_INC) -c $< -o $@
+	$(CC) $(CFLAGS) $(LIBFT_INC) -c $< -o $@
 
-$(PRINTF_LIB):
-	make -C $(PRINTF_PATH)
+# $(PRINTF_LIB):
+# 	make -C $(PRINTF_PATH)
+
+$(LIBFT_LIB):
+	make -C $(LIBFT_PATH)
 
 clean:
 	rm -f $(SERVER_OBJS) $(CLIENT_OBJS)
-	make -C $(PRINTF_PATH)
+	make -C $(LIBFT_PATH)
 
 fclean: clean
-	rm -f $(SERVER_EXE) $(CLIENT_EXE)
-	make -C $(PRINTF_PATH) fclean
+	rm -f $(SERVER_EXE) $(CLIENT_EXE) server client
+	make -C $(LIBFT_PATH) fclean
+
+# clean:
+# 	rm -f $(SERVER_OBJS) $(CLIENT_OBJS)
+# 	make -C $(PRINTF_PATH) $(LIBFT_PATH)
+
+# fclean: clean
+# 	rm -f $(SERVER_EXE) $(CLIENT_EXE)
+# 	make -C $(PRINTF_PATH) $(LIBFT_PATH) fclean
 
 re: fclean all
 
--- server.c
+++ server.c
@@ -3,90 +3,129 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomtom <tomtom@student.42.fr>              +#+  +:+       +#+        */
+/*   By: thobenel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/23 12:07:07 by thobenel          #+#    #+#             */
-/*   Updated: 2024/08/23 21:36:08 by tomtom           ###   ########.fr       */
+/*   Created: 2024/08/31 12:15:54 by thobenel          #+#    #+#             */
+/*   Updated: 2024/08/31 12:17:25 by thobenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
-
-#include <unistd.h>
+#include "libft/libft.h"
 #include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-
-static void	ft_tyesfada(char c, int fd)
+static void	ft_handle(int signal, siginfo_t *info, void *txt)
 {
-	write(fd, &c, 1);
-}
+	static int				bits = 0;
+	static __pid_t		pid = 0;
+	static unsigned char		carac = 0;
 
-static void	ft_handle(int sig, siginfo_t *info, void *txt)
-{
-	static unsigned char	str;
-	static int				i;
-	static __pid_t			pid;
-
+	// bits = 0;
+	// pid = 0;
+	// carac = 0;
 	(void)txt;
-	i = 0;
-	pid = 0;
-	str = 0;
 	if (!pid)
 		pid = info->si_pid;
-	str |= (sig == SIGUSR2);
-	i++;
-	if (i == 8)
+	carac |= (signal == SIGUSR2);
+	if (++bits == 8)
 	{
-		i = 0;
-		if (!str)
+		bits = 0;
+		if (!carac)
 		{
 			kill(pid, SIGUSR2);
 			pid = 0;
 			return ;
 		}
-		ft_tyesfada(str, 1);
-		str = 0;
+		ft_putchar_fd(carac, 1);
+		carac  = 0;
 		kill(pid, SIGUSR1);
 	}
 	else
-		str <<= 1;
-}
-
-void	ft_baniere(void)
-{
-	ft_printf("*------------------------------------------*\n");
-	ft_printf("|                                          |\n");
-	ft_printf("|            Welcome to MiniTalk           |\n");
-	ft_printf("|                                          |\n");
-	ft_printf("*------------------------------------------*\n");
-	ft_printf("\n\n");
-}
-
-void	ft_pid(void)
-{
-	ft_printf("*------------------------------------------*\n");
-	ft_printf("|            Serveur PID: %d           |\n", getpid());
-	ft_printf("*------------------------------------------*\n");
-	ft_printf("\n\n");
+		carac <<= 1;
 }
 
 int	main(void)
 {
-	struct sigaction s_sigaction;
+	struct sigaction	s_sigaction;
 
-	ft_baniere();
-	ft_pid();
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
 	s_sigaction.sa_sigaction = ft_handle;
 	s_sigaction.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &s_sigaction, 0);
 	sigaction(SIGUSR2, &s_sigaction, 0);
-	ft_printf("*------------------------------------------*\n");
-	ft_printf("|            Waiting for message...        |\n");
-	ft_printf("|                                          |\n");
-	ft_printf("|                                          |\n");
 	while (1)
 		pause();
 	return (0);
 }
+
+// static void	ft_handle(int sig, siginfo_t *info, void *txt)
+// {
+// 	static unsigned char	str;
+// 	static int				i;
+// 	static __pid_t			pid;
+
+// 	(void)txt;
+// 	i = 0;
+// 	pid = 0;
+// 	str = 0;
+// 	if (!pid)
+// 		pid = info->si_pid;
+// 	str |= (sig == SIGUSR2);
+// 	if (++i == 8)
+// 	{
+// 		i = 0;
+// 		if (!str)
+// 		{
+// 			kill(pid, SIGUSR2);
+// 			pid = 0;
+// 			return ;
+// 		}
+// 		ft_putchar_fd(str, 1);
+// 		str = 0;
+// 		kill(pid, SIGUSR1);
+// 	}
+// 	else
+// 		str <<= 1;
+// }
+
+
+
+
+
+// void	ft_baniere(void)
+// {
+// 	ft_putstr_fd("*------------------------------------------*\n", 1);
+// 	ft_putstr_fd("|                                          |\n", 1);
+// 	ft_putstr_fd("|            Welcome to MiniTalk           |\n", 1);
+// 	ft_putstr_fd("|                                          |\n", 1);
+// 	ft_putstr_fd("*------------------------------------------*\n", 1);
+// 	ft_putstr_fd("\n", 1);
+// }
+
+// // ft_putstr_fd("*------------------------------------------*\n", 1);
+// // ft_putstr_fd("|            Serveur PID: &i           |\n", i);
+// // ft_putstr_fd("*------------------------------------------*\n", 1);
+
+// int	main(void)
+// {
+// 	struct sigaction	sa;
+
+// 	ft_baniere();
+// 	ft_putstr_fd("		Your PID: ", 1);
+// 	ft_putnbr_fd(getpid(), 1);
+// 	sa.sa_sigaction = ft_handle;
+// 	sa.sa_flags = SA_SIGINFO;
+// 	sigaction(SIGUSR1, &sa, 0);
+// 	sigaction(SIGUSR2, &sa, 0);
+// 	ft_putstr_fd("*------------------------------------------*\n", 1);
+// 	ft_putstr_fd("|            Waiting for message...        |\n", 1);
+// 	ft_putstr_fd("|                                          |\n", 1);
+// 	ft_putstr_fd("|                                          |\n", 1);
+// 	while (1)
+// 		pause();
+// 	return (0);
+// }
