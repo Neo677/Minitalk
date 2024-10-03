5bf9ec78c57451502ddc245f75a2933ce152668b
thobenel <grandmasterchenn@gmail.com>
2024-10-03 16:53:26 +0200

    EWQ

--- client.c
+++ client.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomtom <tomtom@student.42.fr>              +#+  +:+       +#+        */
+/*   By: thobenel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/07 18:11:47 by thobenel          #+#    #+#             */
-/*   Updated: 2024/09/11 15:21:28 by tomtom           ###   ########.fr       */
+/*   Created: 2024/10/02 13:23:17 by thobenel          #+#    #+#             */
+/*   Updated: 2024/10/02 13:23:19 by thobenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 volatile sig_atomic_t	g_ack_receveid = 0;
+// Acknowledgment (accusé de réception)
 
 static void	ft_counter(int num)
 {
@@ -25,6 +26,8 @@ static void	ft_counter(int num)
 		i++;
 	}
 }
+//			lorsque le SIGUSR1 est recu alors on incr une var i
+//			et print la valeur du signal recu
 
 static int	error_cheker(int ac)
 {
@@ -32,6 +35,8 @@ static int	error_cheker(int ac)
 		return (1);
 	return (0);
 }
+
+//		check le nb arg est bien 3
 
 static int	send_lengh(pid_t server_pid, int len)
 {
@@ -52,6 +57,11 @@ static int	send_lengh(pid_t server_pid, int len)
 	}
 	return (0);
 }
+
+// envoie la longueur du message (len) au serveur, bit par bit.
+// utilise un entier de 32 bits pour encoder la longueur et envoie chaque bit
+// en utilisant des signaux SIGUSR1 (pour le bit 0) et SIGUSR2 (pour le bit 1).
+// attend un accusé de réception (ACK) entre chaque envoi.
 
 static int	send_message(pid_t server_pid, char *msg, int delay)
 {
@@ -79,6 +89,12 @@ static int	send_message(pid_t server_pid, char *msg, int delay)
 	return (0);
 }
 
+/*  Cette fonction envoie chaque caractère du message au serveur, bit par bit.
+utilise SIGUSR1 pour envoyer un bit de valeur 0 
+et SIGUSR2 pour un bit de valeur 1.
+   Un accusé de réception (ACK) est attendu après l'envoi de chaque bit.
+*/
+
 int	main(int ac, char **av)
 {
 	pid_t	server_pid;
@@ -87,7 +103,8 @@ int	main(int ac, char **av)
 	int		delay;
 
 	if (error_cheker(ac))
-		return (ft_printf("Utilise PID = ***** || Message == *****\n"), 1);
+		return (ft_printf("Utilise ton PID = ***** et ton Message == *****\n"),
+			1);
 	server_pid = ft_atoi(av[1]);
 	if (server_pid == 0 || kill(server_pid, 0) == -1)
 		return (ft_printf("Process with this id doesn't exist\n"), 1);
@@ -101,6 +118,43 @@ int	main(int ac, char **av)
 	send_message(server_pid, message, delay);
 	return (0);
 }
+
+/*
+int	main(int ac, char **av)
+{
+	pid_t	server_pid;
+	char	*message;
+	int		len;
+	int		delay;
+
+	if (error_cheker(ac))
+		return (ft_printf("Utilise ton PID = ***** et ton Message == *****\n"),
+			1);
+		// check le bon fonctionement des args
+	server_pid = ft_atoi(av[1]);
+		// convertie le pid en entier
+	if (server_pid == 0 || kill(server_pid, 0) == -1)
+		return (ft_printf("Process with this id doesn't exist\n"), 1);
+		// check si le pid est correct
+	message = av[2];
+		// stock le message
+	len = ft_strlen(message);
+		//stock sa longueur
+	delay = 50;
+		// cree un delai pour son envoie
+	if (len > 12000)
+		delay = 150;
+		// si le delaie est
+			+ grand alors on augmente en moyenne la longueur est de 10.000
+	signal(SIGUSR1, ft_counter);
+		// Attache SIGUSR1 à la fonction ft_counter pour gérer les Acknowledgment
+	send_lengh(server_pid, len);
+		// envoie la longeur du message
+	send_message(server_pid, message, delay);
+		// envoie le message
+	return (0);
+}
+*/
 
 /*static int	ft_send_count(pid_t pid, int len)
 {
@@ -147,30 +201,6 @@ static int	handle_bad_enter(int ac)
 {
 	if (ac != 3)
 		return (1);
-	return (0);
-}
-
-int	main(int ac, char **av)
-{
-	int			len;
-	int			waiter;
-	char		*str;
-	__pid_t	pid;
-
-	if (handle_bad_enter(ac))
-		return (ft_printf("God damn bro you cant even enter
-			and id and a message ???\n seriously try again or you regret it\n"),
-1);
-	pid = ft_atoi(av[1]);
-	if (pid == 0 || kill(pid, 0))
-		return (printf("The ID is fake bro\n"), 1);
-	str = av[2];
-	len = ft_strlen(str);
-	waiter = 50;
-	if (len > 12000)
-		waiter = 150;
-	ft_send_count(pid, len);
-	ft_send_bits(pid, str, waiter);
 	return (0);
 }
 
--- makefile
+++ makefile
@@ -12,11 +12,6 @@
 
 CC = cc
 CFLAGS = -Wall -Werror -Wextra
-
-# PRINTF_PATH = ft_printf
-# PRINTF_LIB = $(PRINTF_PATH)/ft_printf.a
-# PRINTF_INC = -I$(PRINTF_PATH)
-
 LIBFT_PATH = libft
 LIBFT_LIB = $(LIBFT_PATH)/libft.a
 LIBFT_INC = -I$(LIBFT_PATH)
@@ -26,49 +21,111 @@ CLIENT = client.c
 SERVER_EXE = $(SERVER:.c=.o)
 CLIENT_EXE = $(CLIENT:.c=.o)
 
+# Couleurs
+GREEN = \033[0;32m
+YELLOW = \033[0;33m
+BLUE = \033[0;34m
+RED = \033[0;31m
+RESET = \033[0m
+
 all: server client
 
 server: $(SERVER_EXE) $(LIBFT_LIB)
+	@echo "$(YELLOW)Compiling server...$(RESET)"
 	$(CC) $(CFLAGS) $(SERVER_EXE) -o server -L$(LIBFT_PATH) -lft
+	@echo "$(GREEN)Server compilation complete!$(RESET)"
 
 client: $(CLIENT_EXE) $(LIBFT_LIB)
+	@echo "$(YELLOW)Compiling client...$(RESET)"
 	$(CC) $(CFLAGS) $(CLIENT_EXE) -o client -L$(LIBFT_PATH) -lft
-
-
-# server: $(SERVER_EXE) $(PRINTF_LIB) $(LIBFT_LIB)
-# 	$(CC) $(CFLAGS) $(SERVER_EXE) -o server -L$(PRINTF_PATH) -l:ft_printf.a 
-
-# client: $(CLIENT_EXE) $(PRINTF_LIB) $(LIBFT_LIB)
-# 	$(CC) $(CFLAGS) $(CLIENT_EXE) -o client -L$(PRINTF_PATH) -l:ft_printf.a 
-
-# %.o: %.c
-# 	$(CC) $(CFLAGS) $(PRINTF_INC) -c $< -o $@
+	@echo "$(GREEN)Client compilation complete!$(RESET)"
 
 %.o: %.c
+	@echo "$(BLUE)Compiling $< into object file...$(RESET)"
 	$(CC) $(CFLAGS) $(LIBFT_INC) -c $< -o $@
 
-# $(PRINTF_LIB):
-# 	make -C $(PRINTF_PATH)
-
 $(LIBFT_LIB):
+	@echo "$(YELLOW)Compiling libft...$(RESET)"
 	make -C $(LIBFT_PATH)
+	@echo "$(GREEN)Libft compilation complete!$(RESET)"
 
 clean:
-	rm -f $(SERVER_OBJS) $(CLIENT_OBJS)
+	@echo "$(RED)Cleaning up object files...$(RESET)"
+	rm -f $(SERVER_EXE) $(CLIENT_EXE)
 	make -C $(LIBFT_PATH) clean
+	@echo "$(RED)Clean complete!$(RESET)"
 
 fclean: clean
-	rm -f $(SERVER_EXE) $(CLIENT_EXE) server client
+	@echo "$(RED)Removing executables...$(RESET)"
+	rm -f server client
 	make -C $(LIBFT_PATH) fclean
-
-# clean:
-# 	rm -f $(SERVER_OBJS) $(CLIENT_OBJS)
-# 	make -C $(PRINTF_PATH) $(LIBFT_PATH)
-
-# fclean: clean
-# 	rm -f $(SERVER_EXE) $(CLIENT_EXE)
-# 	make -C $(PRINTF_PATH) $(LIBFT_PATH) fclean
+	@echo "$(RED)Full clean complete!$(RESET)"
 
 re: fclean all
 
 .PHONY: all clean fclean re
+
+
+# CC = cc
+# CFLAGS = -Wall -Werror -Wextra
+
+# # PRINTF_PATH = ft_printf
+# # PRINTF_LIB = $(PRINTF_PATH)/ft_printf.a
+# # PRINTF_INC = -I$(PRINTF_PATH)
+
+# LIBFT_PATH = libft
+# LIBFT_LIB = $(LIBFT_PATH)/libft.a
+# LIBFT_INC = -I$(LIBFT_PATH)
+
+# SERVER = server.c
+# CLIENT = client.c
+# SERVER_EXE = $(SERVER:.c=.o)
+# CLIENT_EXE = $(CLIENT:.c=.o)
+
+
+# all: server client
+
+# server: $(SERVER_EXE) $(LIBFT_LIB)
+# 	$(CC) $(CFLAGS) $(SERVER_EXE) -o server -L$(LIBFT_PATH) -lft
+
+# client: $(CLIENT_EXE) $(LIBFT_LIB)
+# 	$(CC) $(CFLAGS) $(CLIENT_EXE) -o client -L$(LIBFT_PATH) -lft
+
+
+# # server: $(SERVER_EXE) $(PRINTF_LIB) $(LIBFT_LIB)
+# # 	$(CC) $(CFLAGS) $(SERVER_EXE) -o server -L$(PRINTF_PATH) -l:ft_printf.a 
+
+# # client: $(CLIENT_EXE) $(PRINTF_LIB) $(LIBFT_LIB)
+# # 	$(CC) $(CFLAGS) $(CLIENT_EXE) -o client -L$(PRINTF_PATH) -l:ft_printf.a 
+
+# # %.o: %.c
+# # 	$(CC) $(CFLAGS) $(PRINTF_INC) -c $< -o $@
+
+# %.o: %.c
+# 	$(CC) $(CFLAGS) $(LIBFT_INC) -c $< -o $@
+
+# # $(PRINTF_LIB):
+# # 	make -C $(PRINTF_PATH)
+
+# $(LIBFT_LIB):
+# 	make -C $(LIBFT_PATH)
+
+# clean:
+# 	rm -f $(SERVER_OBJS) $(CLIENT_OBJS)
+# 	make -C $(LIBFT_PATH) clean
+
+# fclean: clean
+# 	rm -f $(SERVER_EXE) $(CLIENT_EXE) server client
+# 	make -C $(LIBFT_PATH) fclean
+
+# # clean:
+# # 	rm -f $(SERVER_OBJS) $(CLIENT_OBJS)
+# # 	make -C $(PRINTF_PATH) $(LIBFT_PATH)
+
+# # fclean: clean
+# # 	rm -f $(SERVER_EXE) $(CLIENT_EXE)
+# # 	make -C $(PRINTF_PATH) $(LIBFT_PATH) fclean
+
+# re: fclean all
+
+# .PHONY: all clean fclean re
